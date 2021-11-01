a67eeaf6b19241e27d7b5be092d769c0504c9876
Jacky <>
2021-10-31 22:16:54 -0600

    Adding some middleware for requests made to the server. Had to enable options so that the FE could make requests without cors errors

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,20 @@ app.get('/', (_, res) => {
 	res.json({ message: 'fwi-poker-api' });
 });
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+
+	next();
+});
+
 app.use('/players', PlayerRouter);
 
 app.listen(3001, () => {
